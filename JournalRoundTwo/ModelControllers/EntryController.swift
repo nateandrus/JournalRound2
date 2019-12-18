56c1af2c6a9506ce07bf096265db1bef30a618f6
Nathan Andrus <nathanpandrus@gmail.com>
2019-12-18 11:14:55 -0700

    Add model controller

---
+++ JournalRoundTwo/ModelControllers/EntryController.swift
@@ -0,0 +1,26 @@
+//
+//  EntryController.swift
+//  JournalRoundTwo
+//
+//  Created by Nathan Andrus on 12/18/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import Foundation
+
+class EntryController {
+    
+    static let shared = EntryController()
+    
+    func createEntry(title: String, text: String) {
+        
+    }
+    
+    func updateEntry() {
+        
+    }
+    
+    func deleteEntry() {
+        
+    }
+}
